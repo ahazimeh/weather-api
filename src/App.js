72e0cd2fb7f1767f50ad6cb3539e43c7c016e027
ahazimeh <hazimeh95@gmail.com>
2020-10-12 18:28:21 +0300

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Search from "./components/Search";
-
+import Storm from "./img/weather-icons/storm.svg";
 import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 
@@ -20,10 +20,72 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+      <div>
+        <div className="details">
+          {/* <SayHi /> */}
+          {/* <SayHello color="black" name={this.state.name} /> */}
+          <Search handleInput={this.handleInputChange} />
+          <img className="img0" src={require('./img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
+          <div className="DayWeather">partly cloudy</div>
+          <div className="temperature">
+          <div className="tempw">Temperature</div>
+          <div className="temp"> 10<span>&#176;</span>  to 11<span>&#176;</span> C</div>
+          </div>
+          <div className="weather">
+            <div className="humidity">Humidity</div><div className="humidityvalue">78</div>
+            <div className="pressure">Pressure</div><div className="pressurevalue">1008.48</div>
+          </div>
+          <div className="times">
+            <div>
+              <div className="time">03:00</div>
+              <img src={require('./img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
+              <div className="day">8<span>&#176;</span>C</div>
+            </div>
+            <div>
+            <div className="time">06:00</div>
+              <img src={require('./img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
+              <div className="day">9<span>&#176;</span>C</div>
+            </div>
+
+
+            <div>
+            <div className="time">09:00</div>
+              <img src={require('./img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
+              <div className="day">14<span>&#176;</span>C</div>
+            </div>
+            <div>
+            <div className="time">12:00</div>
+              <img src={require('./img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
+              <div className="day">17<span>&#176;</span>C</div>
+            </div>
+
+
+            <div>
+            <div className="time">15:00</div>
+              <img src={require('./img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
+              <div className="day">18<span>&#176;</span>C</div>
+            </div>
+            <div>
+            <div className="time">18:00</div>
+              <img src={require('./img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
+              <div className="day">16<span>&#176;</span>C</div>
+            </div>
+
+            <div>
+            <div className="time">21:00</div>
+              <img src={require('./img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
+              <div className="day">13<span>&#176;</span>C</div>
+              
+            </div>
+
+
+
+
+
+
+          </div>
+          </div>
+      
       </div>
     );
   }
