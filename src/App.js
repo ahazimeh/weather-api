589b8188bcef2cb2f4b865097574e31d67fcc89f
ahazimeh <hazimeh95@gmail.com>
2020-10-14 10:37:49 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import Search from "./components/Search";
+import SayHi, { SayHello } from "./components/WeatherItem";
 import CurrentWeather from "./components/CurrentWeather";
 import DetailsWeather from "./components/DetailsWeather";
-import Storm from "./img/weather-icons/storm.svg";
-import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
@@ -11,8 +10,11 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
-      name: "Ali"
+      name: "Ali",
+      role:undefined,
+      flag:0
     };
   }
 
@@ -20,16 +22,53 @@ class App extends Component {
     this.setState({ name: value });
   };
 
+
+  componentDidMount() {
+    // fetch("http://api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=2e753ccc114a1bd1d02d58d6adaaeda9")
+    
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=2e753ccc114a1bd1d02d58d6adaaeda99")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            role:result,
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+      
+  }
+
   render() {
+    
+    var isLoaded1=false;
+    var { error, isLoaded, items } = this.state;
+    if(isLoaded){
+      var x = this.state.role;
+      // console.log(x);
     var list=fakeWeatherData.list;
     var cur;
     var c = [];
 
+    
     for(var i=0;i<list.length;i++){
-      if( list[i].dt_txt.localeCompare("2017-02-17 03:00:00") == 0)
+      if( list[i].dt_txt.localeCompare("2017-02-17 03:00:00") === 0)
       {
         cur = i;
       }
+      console.log(list[i].main.temp);
+      if(this.state.flag==0){
+      
       list[i].main.temp -=273.15;
       list[i].main.temp_min -=273.15;
       list[i].main.temp_max -=273.15;
@@ -37,47 +76,38 @@ class App extends Component {
       list[i].main.temp = Math.round(list[i].main.temp);
       list[i].main.temp_min = Math.round(list[i].main.temp_min);
       list[i].main.temp_max = Math.round(list[i].main.temp_max);
-
+    }
       if( list[i].dt_txt.includes("2017-02-17"))
       {
         c.push(list[i]);
-        // c[0].main.temp_max=-1;
-
       }
 
 
 
     }
+    this.state.flag=1;        
 
     function sortf(a,b){
-      // Turn your strings into dates, and then subtract them
-      // to get a value that is either negative, positive, or zero.
       return a.dt_txt.localeCompare(b.dt_txt);
     }
     c.sort(sortf);
-    // http://api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=57cf9da04987637a23fcbc26f5356e12
 
+
+    isLoaded1=true;
+  }
+    
+    if (!isLoaded || !isLoaded1) {
+       return <div>Loading...</div>;
+    }
+    else{
     return (
-      <div>
-        {
-          //  c[2].main.temp_max
-          c[1].main.temp
-        }
-        {
-          
-        //cur//list number we will be using
-        }
-        {/* {list[0].main.temp}
-        {fakeWeatherData.city.name} */}
         <div className="details">
-          {/* <SayHi /> */}
-          {/* <SayHello color="black" name={this.state.name} /> */}
           <Search weather={"FIND WEATHER"} handleInput={this.handleInputChange} />
-          <CurrentWeather currentweather = {list[5]} handleInput={this.handleInputChange} />
-          <DetailsWeather details = {c} handleInput={this.handleInputChange} />
+          <CurrentWeather currentweather = {list[cur]} />
+          <DetailsWeather details = {c} />
           </div>
-      </div>
     );
+    }
   }
 }
 
--- src/components/DetailsWeather.js
+++ src/components/DetailsWeather.js
@@ -1,6 +1,4 @@
 import React from "react";
-
-import clear from "../img/weather-icons/clear.svg";
 import "./DetailsWeather.css";
 class DetailsWeather extends React.Component {
   state = {
@@ -8,13 +6,17 @@ class DetailsWeather extends React.Component {
   };
 
   render() {
-    for(var i=0;i<8;i++){
-      {this.props.details[i].dt_txt=this.props.details[i].dt_txt.substr(11,5)}
-    }
+    // for(var i=0;i<8;i++){
+    //   {this.props.details[i].dt_txt=this.props.details[i].dt_txt.substr(11,5)}
+    // }
+    var items = this.props.details.map((detail) => 
+    <li>{detail.dt_txt.substr(11,5)}</li>
+    );
+    console.log(items[0]._self.props.details[1].dt_txt.substr(11,5));
     var l = [];
     for(var i=1;i<8;i++){
       l.push(<div>
-        <div className="time">{this.props.details[i].dt_txt}</div>
+        <div className="time">{items[0]._self.props.details[i].dt_txt.substr(11,5)}</div>
           <img src={require('../img/weather-icons/mostlycloudy.svg')} alt="storm icon" />
           <div className="day">{this.props.details[i].main.temp}<span>&#176;</span>C</div>
         </div>);
