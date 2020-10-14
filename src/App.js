15022be3fa386f79d81e234d6f53b0dda77c3159
ahazimeh <hazimeh95@gmail.com>
2020-10-14 13:09:06 +0300

    updated the code

--- src/App.js
+++ src/App.js
@@ -12,21 +12,29 @@ class App extends Component {
     super(props);
     
     this.state = {
-      name: "Beirut",
+      name: "London",
       role:undefined,
-      flag:0
+      flag:0,
+      flagCity:0
     };
   }
 
   handleInputChange = value => {
     this.setState({ name: value });
-    this.componentDidMount();
+    this.componentDidMount(value);
 
   };
 
 
-  componentDidMount() {
+  componentDidMount(e) {
+    console.log(e);
+    // ffad005b341ddc6ee289355588b4669e
     // fetch("http://api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=2e753ccc114a1bd1d02d58d6adaaeda9")
+    // fetch("http://api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=ffad005b341ddc6ee289355588b4669e")
+    // fetch("http://api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=9f89982b3e80a5be4d59b86343791756")
+    // this.props.flagCity=1;
+    if(this.state.flagCity==0){
+      this.state.flagCity = 1;
     fetch("http://api.openweathermap.org/data/2.5/forecast?q="+this.state.name+"&cnt=8&units=metric&appid=2e753ccc114a1bd1d02d58d6adaaeda9")
       .then(res => res.json())
       .then(
@@ -47,26 +55,63 @@ class App extends Component {
           });
         }
       );
+    }
+
+
+
+
+    else {
+      // this.state.flagCity = 1;
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q="+e+"&cnt=8&units=metric&appid=2e753ccc114a1bd1d02d58d6adaaeda9")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            role:result,
+            isLoaded: true,
+            // items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+    }
+
+
+
+
+
+    
       
   }
 
   render() {
-    this.componentDidMount();
     
+
+    // this.componentDidMount();
+    // alert(this.state.name);
     var isLoaded1=false;
     var { error, isLoaded, items } = this.state;
     if(isLoaded){
       var x = this.state.role;
+      console.log(x);
     var list=x.list;
     var cur;
     var c = [];
 
     
     for(var i=0;i<list.length;i++){
-      if( list[i].dt_txt.localeCompare("2017-02-17 03:00:00") === 0)
-      {
-        cur = i;
-      }
+      // if( list[i].dt_txt.localeCompare("2017-02-17 03:00:00") === 0)
+      // {
+      //   cur = i;
+      // }
       console.log(list[i].main.temp);
       if(this.state.flag==0){
       
